1ab9b8f781429dbdf812b08715bcfd3c23857021
Parvesh Chugani <pcstartups@gmail.com>
2013-07-09 09:57:51 +0000

    Added modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer = fs.readFileSync("index.html");
+var string = buffer.toString("utf8");
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send('string');
 });
 
 var port = process.env.PORT || 5000;
